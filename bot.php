0dfa28720ebeba6787b7aefbd8bb6108ff1a3fb2
Loïc @ Funkyduck <jacques_loic@funkyduck.be>
2025-07-29 21:33:48 +0200

    Add errors logs

--- bot.php
+++ bot.php
@@ -1,5 +1,10 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('log_errors', '1');
+ini_set('error_log', __DIR__ . '/error.log'); // un fichier error.log à la racine du projet
+
 require 'vendor/autoload.php';
 
 use Promptly\Bot;
