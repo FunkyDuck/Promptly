974ddeedcc5ab734598c28000a09d62ed041f0cd
Loïc @ Funkyduck <jacques_loic@funkyduck.be>
2025-07-29 22:26:29 +0200

    Remove loud and clear test message

--- src/CommandHandler.php
+++ src/CommandHandler.php
@@ -12,8 +12,6 @@ class CommandHandler {
 
         $content = $message->content;
 
-        $message->channel->sendMessage('Message reçu loud and clear !');
-
         if(empty($content)) {
             file_put_contents(__DIR__ . '/empty_messages.log', date('c') . " Message vide reçu de {$message->author->username}\n", FILE_APPEND);
             return;
