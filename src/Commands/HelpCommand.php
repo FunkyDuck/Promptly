076b810e9e045677335d7d947829f60b92185a52
Loïc @ Funkyduck <jacques_loic@funkyduck.be>
2025-07-31 23:00:50 +0200

    Improve and update help command

--- src/Commands/HelpCommand.php
+++ src/Commands/HelpCommand.php
@@ -7,6 +7,7 @@ use Discord\Parts\Channel\Message;
 class HelpCommand {
     public function execute(Message $message, array $params) {
         $file = __DIR__ . '/../../resources/help.json';
+        $command = $params[0] ?? null;
 
         if(!file_exists($file)) {
             $message->channel->sendMessage("Oups, l'aide est telle une 404");
@@ -20,12 +21,23 @@ class HelpCommand {
             return;
         }
 
-        $response = "# Commandes disponibles:\n";
+        if($command) {
+            $commandHelp = array_filter($data['help'], fn($c) => strtolower($c['command']) == strtolower($command));
 
-        foreach($data['help'] as $entry) {
-            $response .= "• `{$entry['command']}` : _{$entry['description']}_\n";
+            if(!$commandHelp) {
+                $message->channel->sendMessage("🧑‍🦯 La seule personne a avoir vu cette commande, c'est Gilbert Montagné.\n_Commande `{$command}`inexistante_");
+                return;
+            }
+            $response = "#Commande `{$command}`:\n{$commandHelp['description']}\n_{$commandHelp['details']}_";
         }
-
-        $message->channel->sendMessage($response);
+        else {
+            $response = "# Commandes disponibles:\n";
+            
+            foreach($data['help'] as $entry) {
+                $response .= "• `{$entry['command']}` : _{$entry['description']}_\n";
+            }
+            
+            $message->channel->sendMessage($response);
+        }
     }
 }
